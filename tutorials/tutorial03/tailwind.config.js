7e3658d5f790837a275c13431d27e6e3953edc14
Apiznel <cchin2@unca.edu>
2026-02-06 14:54:57 -0500

    Tutorial 3: CSS Frameworks (Tailwind)

---
+++ tutorials/tutorial03/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./*.html", "./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "custom-blue": "#00BAFF",
+        "custom-purple": "#6336FA",
+      },
+      fontFamily: {
+        Poppins: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
+
